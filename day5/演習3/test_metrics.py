a1144abce97b9460ffd140332a91584eaf40015e
Takeshi0805 <kojima-t@diamond.co.jp>
2025-05-15 23:03:53 +0900

    Apply black formatting

--- day5/演習3/test_metrics.py
+++ day5/演習3/test_metrics.py
@@ -1,18 +1,22 @@
 # day5/演習3/tests/test_metrics.py
 import pandas as pd
 
+
 def test_metrics_file_exists():
     # metrics.csv が生成されていること
     import os
-    assert os.path.isfile('metrics.csv')
+
+    assert os.path.isfile("metrics.csv")
+
 
 def test_logistic_accuracy_threshold():
-    df = pd.read_csv('metrics.csv')
-    lr_acc = df.loc[df['model']=='LogisticRegression','accuracy'].iloc[0]
+    df = pd.read_csv("metrics.csv")
+    lr_acc = df.loc[df["model"] == "LogisticRegression", "accuracy"].iloc[0]
     assert lr_acc >= 0.75
 
+
 def test_randomforest_beats_logistic():
-    df = pd.read_csv('metrics.csv')
-    rf_acc = df.loc[df['model']=='RandomForest','accuracy'].iloc[0]
-    lr_acc = df.loc[df['model']=='LogisticRegression','accuracy'].iloc[0]
+    df = pd.read_csv("metrics.csv")
+    rf_acc = df.loc[df["model"] == "RandomForest", "accuracy"].iloc[0]
+    lr_acc = df.loc[df["model"] == "LogisticRegression", "accuracy"].iloc[0]
     assert rf_acc > lr_acc
